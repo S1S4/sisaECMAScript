70d0942ac166be60bca6eda3598fee4c48a5a4ee
S1S4 <Cesar_Zhiro26@hotmail.com>
2022-09-10 18:29:23 -0400

    agregada clase de spread operator al es6/04.js

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,22 @@ console.log(a, b);
 let user = { username: 'Sisa', age: 22};
 let { username, age } = user;
 console.log(username, age);
+
+
+// spread operator = desestructura o deconstruye
+
+let person = { name: 'sisa', age: 23498 };
+let country = 'VE';
+
+let data = { id: 1, ...person, country }; //con los ...person asignas todo lo que se encuentre en el objeto person
+console.log(data);
+
+// rest empaqueta o construye
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0]; //suma num + el primer valor del array dentro de values
+}
+
+sum(1, 1, 2, 3);
