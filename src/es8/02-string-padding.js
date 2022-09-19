b3a20b3f250bf49b3fc9b41d4e6cb83602175a45
S1S4 <Cesar_Zhiro26@hotmail.com>
2022-09-19 17:24:50 -0400

    agregado es8/string-padding y trailling-commas

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,5 @@
+const string = 'texto pepesco';
+
+console.log(string.padStart(22, 'for Sisa ')); /*entregando un string de N elementos y 
+los rellena con la palabra que pasamos con padStart*/
+console.log(string.padEnd(31, ' to all developers'));
---
+++ src/es8/03-trailling-commas.js
@@ -0,0 +1,4 @@
+const array = [22, 33, 54, 41, 81, , , , , , ,];
+
+console.log(array);
+console.log(array.length);
