1d5bcbaab8c32b1f1c26191f0b7eaeb6ef7cfc9e
S1S4 <Cesar_Zhiro26@hotmail.com>
2022-09-19 14:31:05 -0400

    agregado es7/exponential y array-includes

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5, 6];
+
+console.log(numbers.includes(9));
+
+const list = ['sisa', 'pepe', 'pocho'];
+
+console.log(list.includes('sisa')); //si yo pusiera Sisa en el includes mandaria false pues el string es distinto
