b99a4bacd4e0e9c3ea7f4bff9318696ccb106675
S1S4 <Cesar_Zhiro26@hotmail.com>
2022-09-19 20:39:17 -0400

    comentario en es11/promise-allsettled

--- src/es11/03-promise-allsettled.js
+++ src/es11/03-promise-allsettled.js
@@ -4,3 +4,9 @@ const promise3 = new Promise((resolve, reject) => resolve('resolve 2'));
 
 Promise.allSettled([promise1, promise2, promise3])
     .then(response => console.log(response));
+
+
+/* se puede usar tambien Promise.all(iterable) 
+La diferencia con
+Promise.allSettled()
+Ejecuta todas las promesas sin importar si son rechazadas o no.*/
